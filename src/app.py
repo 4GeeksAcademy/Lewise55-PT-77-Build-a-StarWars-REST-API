94437e5eb5a15a008547a805f5fd6319edd41a75
Lewise55 <157831388+Lewise55@users.noreply.github.com>
2025-04-30 17:35:12 +0000

    first commit added models favorites, people, and planets + added get, post, and delete requests)

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Favorites, People, Planets
 #from models import Person
 
 app = Flask(__name__)
@@ -37,13 +37,105 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def handle_get_user():
+    users = User.query.all()
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    return jsonify(users), 200
 
-    return jsonify(response_body), 200
+@app.route('/user', methods=['GET'])
+def handle_user_favs():
+    body = request.get_json()
+    user_login = body['login']
+    
+    user = User.query.filter_by(login = user_login).first()
+    favorites = user['favorites']
+
+    return jsonify(favorites), 200
+
+@app.route('/people', methods=['GET'])
+def handle_get_people():
+    people = People.query.all()
+
+    return jsonify(people), 200
+
+@app.route('/people/<int:People_id>', methods=['GET'])
+def handle_get_person(People_id):
+    people = People.query.filter_by(id = People_id).first()
+
+    return jsonify(people), 200
+
+@app.route('/favorite/people/<int:People_id>', methods=['POST'])
+def handle_fav_person(People_id):
+    body = request.get_json()
+    user_id = body['id']
+
+    newFav = Favorites(user_id = user_id, person_id = People_id, planet_id = None )
+    db.session.add(newFav)
+    db.session.commit(newFav)
+
+    return jsonify('Favorite has been created'), 200
+
+@app.route('/favorite/people/<int:People_id>', methods=['DELETE'])
+def handle_delete_fav_person(People_id):
+    body = request.get_json()
+    user_id = body['id']
+
+    fav = Favorites.query.filter_by(user_id = user_id, person_id = People_id).first()
+    db.session.delete(fav)
+    db.session.commit()
+
+    return jsonify('Favorite has been created'), 200
+
+@app.route('/planets', methods=['GET'])
+def handle_get_planets():
+    planets = Planets.query.all()
+
+    return jsonify(planets), 200
+
+@app.route('/planets/<int:Planet_id>', methods=['GET'])
+def handle_get_each_planet(Planet_id):
+    planet = Planets.query.filter_by(id = Planet_id)
+
+    return jsonify(planet), 200
+
+@app.route('/favorite/planet/<int:Planet_id>', methods=['POST'])
+def handle_fav_planet(Planet_id):
+    body = request.get_json()
+    user_id = body['id']
+
+    newFav = Favorites(user_id = user_id, planet_id = Planet_id, People_id = None )
+    db.session.add(newFav)
+    db.session.commit(newFav)
+
+    return jsonify('Favorite has been created'), 200
+
+@app.route('/favorite/planet/<int:Planet_id>', methods=['DELETE'])
+def handle_delete_fav_planet(Planet_id):
+    body = request.get_json()
+    user_id = body['id']
+
+    fav = Favorites.query.filter_by(user_id = user_id, planet_id = Planet_id).first()
+    db.session.delete(fav)
+    db.session.commit()
+
+    return jsonify('Favorite has been created'), 200
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
@@ -9,6 +9,7 @@ class User(db.Model):
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    favorites: Mapped[list["Favorites"]] = relationship(back_populates="user_favorite")
 
 
     def serialize(self):
@@ -16,4 +17,54 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+    
+class Favorites(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    User_id: Mapped[int] = mapped_column(ForeignKey("User.id"))
+    user_favorites: Mapped['User'] = relationship(back_populates="favorites")
+
+    People_id: Mapped[int] = mapped_column(ForeignKey("People.id"))
+    people_favorites: Mapped['People'] = relationship(back_populates="favorites")
+
+    Planet_id: Mapped[int] = mapped_column(ForeignKey("Planets.id"))
+    planet_favorites: Mapped['Planets'] = relationship(back_populates="favorites")
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "User_id": self.User_id,
+            "People_id": self.People_id,
+            "Planet_id": self.Planet_id
+        }
+    
+class People(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    eye_color: Mapped[str] = mapped_column(String(120), nullable=False)
+    hair_color: Mapped[str] = mapped_column(String(120), nullable=False)
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "eye_color": self.eye_color,
+            "hair_color": self.hair_color
+        }
+    
+class Planets(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    climate: Mapped[str] = mapped_column(String(120), nullable=False)
+    terrain: Mapped[str] = mapped_column(String(120), nullable=False)
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "terrain": self.terrain
         }
